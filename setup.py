cc87c2d71ed9d6934d31cf5be71490ade9c09256
Mark Roddy <markroddy@gmail.com>
2024-11-08 20:32:36 +0000

    Bump version number so we can do a release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
         setup(
                 name="replgpt",
-                version="0.1.0",
+                version="0.1.1",
                 description="An interactive REPL with GPT-based assistance",
                 long_description=open("README.md").read(),
                 long_description_content_type="text/markdown",
